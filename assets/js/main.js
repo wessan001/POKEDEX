76a0f0692e5533136c517c54ccafca7a4a573a94
wessan001 <wesleyortegassan@gmail.com>
2023-03-31 15:25:19 -0300

    Pokedex Project in HTML, CSS and JS

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+
+const offset = 0
+const limit = 10
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
+fetch(url)
+    .then(function (response) {
+        response.json().then(function(responseBody) {
+            console.log(responseBody)
+        })
+})
+    .catch (function (error) {
+        console.error(error)
+    })
+    .finally (function (){
+        console.log('Requisicao Concluida')
+    })
+
